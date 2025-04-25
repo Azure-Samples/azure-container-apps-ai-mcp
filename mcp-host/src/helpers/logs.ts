cd5884578b94d55205a4c9026c55467e3a43db1d
Wassim Chegham <github@wassim.dev>
2025-04-25 11:26:02 +0200

    feat: disable debug info in mcp host by default

--- mcp-host/src/helpers/logs.ts
+++ mcp-host/src/helpers/logs.ts
@@ -1,8 +1,6 @@
 import chalk from 'chalk';
 import debug from 'debug';
 
-debug.enable('mcp:*');
-
 export const logger = (namespace: string) => {
   const dbg = debug('mcp:' + namespace);
   const log = (...args: any[]) => {
@@ -16,40 +14,44 @@ export const logger = (namespace: string) => {
     });
     const formattedMessage = formattedArgs.join(' ');
     const message = `${timestamp} ${formattedMessage}`;
-    dbg(message);
-  }
+    dbg(chalk.dim.yellow(message));
+  };
 
   return {
     info(message: string, ...args: any[]) {
-      log(chalk.blue(`${message}`), ...args);
+      log(chalk.dim.yellow(`${message}`), ...args);
     },
     success(message: string, ...args: any[]) {
-      log(chalk.green(`${message}`), ...args);
+      log(chalk.dim.yellow(`${message}`), ...args);
     },
     warn(message: string, ...args: any[]) {
-      log(chalk.yellow(`${message}`), ...args);
+      log(chalk.dim.yellow(`${message}`), ...args);
     },
     error(message: string, ...args: any[]) {
-      log(chalk.red(`${message}`), ...args);
+      log(chalk.dim.yellow(`${message}`), ...args);
     },
-    user(message?: string) {
-      return chalk.magenta(`${message ?? '>>>'} `);
+    user() {
+      return chalk.bold.magenta(`\n User:`) + ' ';
     },
     agent(message: string) {
-      return chalk.blue(`Agent: ${message}`) + '\n';
+      return (
+        chalk.bold.blue.green(`Agent:`) + ' ' + chalk.bold.blue.green(message)
+      );
     },
     thinking(message = 'Agent is thinking...') {
       const delay = 100;
       const chars = ['⠙', '⠘', '⠰', '⠴', '⠤', '⠦', '⠆', '⠃', '⠋', '⠉'];
       let x = 0;
       const timer = setInterval(function () {
-        process.stdout.write(chalk.blue('\r' + chars[x++] + ' ' + message));
+        process.stdout.write(
+          chalk.bold.green(message + '\r' + chars[x++] + ' ')
+        );
         x = x % chars.length;
       }, delay);
 
       return function () {
         clearInterval(timer);
-        process.stdout.write('\r' + ' '.repeat(30) + '\r');
+        process.stdout.write(' '+ '\r' + ' '.repeat(30) + '\r');
       };
     },
   };
